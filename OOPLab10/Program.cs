ee2f1b090d6acfa068751335218156df5ebdb5f9
Mykola Kovshar <knikolay20031230@gmail.com>
2023-04-01 16:54:48 +0300

    Complete first task

---
+++ OOPLab10/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OOPLab10
+{
+    internal class Program
+    {
+        public static Queue<int> _queue = new Queue<int>();
+
+
+        static void Main(string[] args)
+        {
+            object locker = new object();
+
+            Thread thread1 = new Thread(() =>
+            {
+                Random rand = new Random();
+                lock (_queue)
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name);
+                    for (int i = 0; i < 10; i++)
+                    {
+                        _queue.Enqueue(rand.Next(-100, 101));
+                    }
+                }
+
+            });
+            thread1.Name = "Thread 1";
+            Thread thread2 = new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine(Thread.CurrentThread.Name);
+                foreach(int number in _queue)
+                {
+                    Console.Write($"{number} | ");
+                }
+            });
+            thread2.Name = "Thread 2";
+
+            thread1.Start();
+            thread2.Start();
+            
+        }
+    }
+}
